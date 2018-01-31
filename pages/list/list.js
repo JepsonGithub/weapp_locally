c666fb6fc6bcad17b8052ba6ccf73d200d12b61f
Jepson <547212806@qq.com>
2018-01-31 16:25:24 +0800

    上拉加载更多功能完成

--- pages/list/list.js
+++ pages/list/list.js
@@ -8,17 +8,45 @@ Page({
    */
   data: {
     // 当前页
-    page: 1,
+    currentPage: 0,
     
     // 每页多少条
-    limit: 10,
+    pageSize: 10,
 
     // 当前加载的分类
     category: {},
 
     // 当前分类下的所有店铺
-    shops: []
+    shops: [],
+
+    // 是否正在加载
+    isMoreToLoad: true
   
+  },
+
+  // 加载更多的方法封装
+  loadingMore() {
+
+    this.setData({ currentPage: ++this.data.currentPage } )
+
+    // 请求列表信息, 进行渲染
+    fetch(`categories/${this.data.category.id}/shops`, { _page: this.data.currentPage, _limit: this.data.pageSize }).then(res => {
+      // 获取当前一共多少条数据, 计算出下次是否有更多的数据进行进行渲染了
+      let totalLength = res.header["X-Total-Count"]
+      let totalPages = Math.ceil((totalLength / this.data.pageSize))
+      let isMoreToLoad = this.data.currentPage < totalPages ? true : false
+
+      // 将计算出来是否下次可以加载更多内容同步到数据源中
+      this.setData({ isMoreToLoad : isMoreToLoad } )
+
+      // 将请求到的数据拼接渲染到页面中
+      let shops = res.data
+      shops = shops.concat( this.data.shops )
+
+      // 动态更新商品列表信息
+      this.setData({ shops })
+    })
+
   },
 
   /**
@@ -26,7 +54,8 @@ Page({
    */
   onLoad: function (options) {
     let cat = options.cat || 1;
-    console.log(this.data.shops )
+
+    this.setData({ 'category.id': cat  } )
 
     // 请求标题, 进行渲染
     fetch(`categories/${cat}`).then( res => {
@@ -34,7 +63,7 @@ Page({
       let title = res.data.name
 
       // 动态更新到data中
-      this.setData({ category: { title: title } })
+      this.setData({ 'category.title':  title })
 
       // 动态设置当前页面的标题
       wx.setNavigationBarTitle({
@@ -42,13 +71,8 @@ Page({
       })
     })
 
-    // 请求列表信息, 进行渲染
-    fetch(`categories/${cat}/shops`, { _page: this.page, _limit: this.limit }).then( res => {
-      console.log( res.data )
-      // 动态更新商品列表信息
-      this.setData({ shops: res.data })
-    })
-
+    // 一进来先触发一次加载第一页的数据
+    this.loadingMore()
   },
 
   /**
@@ -59,44 +83,21 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    console.log( "上拉事件触发" )
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
-  },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    if ( this.data.isMoreToLoad ) {
+      this.loadingMore()
+    }
+
   }
+
 })
