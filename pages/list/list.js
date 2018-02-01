6ad388cdee701091592504c0792d73264fbb1579
Jepson <547212806@qq.com>
2018-02-01 16:05:13 +0800

    模板tempalte使用,进行提取wxml,wxss模块

--- pages/list/list.js
+++ pages/list/list.js
@@ -2,6 +2,9 @@ const fetch = require('../../utils/fetch')
 
 // pages/list/list.js
 Page({
+  go(e) {
+    console.log(e.currentTarget.dataset.id)
+  },
 
   /**
    * 页面的初始数据
@@ -9,7 +12,7 @@ Page({
   data: {
     // 当前页
     currentPage: 0,
-    
+
     // 每页多少条
     pageSize: 10,
 
@@ -24,7 +27,7 @@ Page({
 
     // 搜索字段
     queryString: ""
-  
+
   },
 
   // 更新搜索字段, 并重置所有字段
@@ -35,22 +38,22 @@ Page({
   },
 
   // 处理函数
-  inputChangeHandle( e ) {
-    this.setData( { queryString: e.detail.value } )
+  inputChangeHandle(e) {
+    this.setData({ queryString: e.detail.value })
   },
 
   // 加载更多的方法封装
   // loadingMore 会返回一个 promise 对象
   loadingMore() {
 
-    this.setData({ currentPage: ++this.data.currentPage } )
+    this.setData({ currentPage: ++this.data.currentPage })
 
     let queryObj = {};
     queryObj._page = this.data.currentPage
     queryObj._limit = this.data.pageSize
 
     // 判断是否需要加上搜索字段
-    if ( this.data.queryString ) {
+    if (this.data.queryString) {
       queryObj.q = this.data.queryString
     }
 
@@ -62,11 +65,11 @@ Page({
       let isMoreToLoad = currentPage < Math.ceil((totalLength / pageSize)) ? true : false
 
       // 将计算出来是否下次可以加载更多内容同步到数据源中
-      this.setData({ isMoreToLoad : isMoreToLoad } )
+      this.setData({ isMoreToLoad: isMoreToLoad })
 
       // 将请求到的数据拼接渲染到页面中
       let shops = res.data
-      shops = shops.concat( this.data.shops )
+      shops = shops.concat(this.data.shops)
 
       // 动态更新商品列表信息
       this.setData({ shops })
@@ -80,15 +83,15 @@ Page({
   onLoad: function (options) {
     let cat = options.cat || 1;
 
-    this.setData({ 'category.id': cat  } )
+    this.setData({ 'category.id': cat })
 
     // 请求标题, 进行渲染
-    fetch(`categories/${cat}`).then( res => {
+    fetch(`categories/${cat}`).then(res => {
       // 拿到标题栏数据, 设置给标题
       let title = res.data.name
 
       // 动态更新到data中
-      this.setData({ 'category.title':  title })
+      this.setData({ 'category.title': title })
 
       // 动态设置当前页面的标题
       wx.setNavigationBarTitle({
@@ -104,14 +107,14 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    console.log( "上拉事件触发" )
+    console.log("上拉事件触发")
 
     // 上拉事件触发, 先重置数据, 再调用 loadingMore 即可
     this.setData({ currentPage: 0, shops: [], isMoreToLoad: true })
@@ -125,7 +128,7 @@ Page({
    */
   onReachBottom: function () {
 
-    if ( this.data.isMoreToLoad ) {
+    if (this.data.isMoreToLoad) {
       this.loadingMore()
     }
 
