013f6eed8492958c2e18ab9059eecb8746b78c85
Jepson <547212806@qq.com>
2018-02-02 10:40:44 +0800

    消息详情页基本完成

--- pages/message/message.js
+++ pages/message/message.js
@@ -5,14 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    messages: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const messages = this.data.messages
+    for (var i = 0; i < 18; i++) {
+      messages.push({
+        title: '免费送票！超有内涵的门票。',
+        date: i + ' September',
+        image: 'https://unsplash.it/400/300',
+        summary: '最糟糕的，也许就是最幸运的。'
+      })
+    }
+
+    this.setData({ messages })
   },
 
   /**
