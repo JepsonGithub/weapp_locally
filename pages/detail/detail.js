6cdf989f03ab3857577c5102dafa88a79cde5617
Jepson <547212806@qq.com>
2018-02-02 10:21:20 +0800

    图片预览功能完成

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -15,58 +15,16 @@ Page({
   onLoad: function (options) {
     let shopid = options.item
     fetch( `shops/${shopid}` ).then( res => {
-      console.log( res.data )
       this.setData({ shop: res.data })
     })
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  // 该方法用于图片预览
+  preview( e ) {
+    wx.previewImage({
+      current: e.currentTarget.dataset.src, // 当前显示图片的http链接
+      urls: this.data.shop.images // 需要预览的图片http链接列表
+    })
   }
 })
